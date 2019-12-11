c7a4aa9e76b871be149b04bbf9bcdbbf7e2dc872
anchung333 <xcessivepupil2@gmail.com>
2019-12-10 20:53:44 -0800

    get ElementsByClassName.js completed

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,31 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
+
+
+// presume there is only ever a single class passed into getElementsByClassName
+var getElementsByClassName = function(className) {
+
+  //have result array
+  var result = [];
+
+  //function that we will recursively call
+  var findAndTraverse = function(element) {
+    //console.log((_.difference(className.split(' '), [...element.classList])).length);
+    // check if the className is in the current element's classList
+    if (element.classList && (_.difference(className.split(' '), [...element.classList])).length === 0) {
+      result.push(element);
+    }
+
+    // check if element has childNodes
+    if (element.childNodes) {
+      _.each(element.childNodes, function(element) {
+        findAndTraverse(element);
+      });
+    }
+  };
+
+  //call the function()
+  findAndTraverse(document.body);
+  return result;
 };
